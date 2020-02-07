1e65e6445808b50eb85882e62e8d1dcd8b5ba34e
Alyssa Colistro <arcolistro@gmail.com>
2020-02-07 11:25:28 -0800

    add code to pass first spec.

--- __tests__/planet-age.test.js
+++ __tests__/planet-age.test.js
@@ -1,10 +1,10 @@
-import { Age } from './src/age.js'
+import { Age } from './../src/age.js';
 import TestRunner from 'jest-runner';
 import { exportAllDeclaration } from '@babel/types';
 
 describe('Age', () => {
     test('should only take a number for age object', () => {
-        const age = new Age('abc');
-        expect(age.earthAge).toEqual("please enter a number");
+        const earthAge = new Age('abc');
+        expect(earthAge.posNumCheck()).toEqual("please enter a number");
     });
 });
--- src/age.js
+++ src/age.js
@@ -1,3 +1,11 @@
-export function Age() {
-    
+export function Age(earthAge) {
+    this.earthAge = earthAge;
 } 
+
+Age.prototype.posNumCheck = function () {
+    if (typeof this.earthAge !== 'number') {
+        return "please enter a number";
+    } else {
+        return true;
+    }
+}
